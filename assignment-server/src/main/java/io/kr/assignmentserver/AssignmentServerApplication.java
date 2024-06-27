2c2f1de8753d468509649f35b8f895cd80f0ed9f
elyudwo <dudqk9696@naver.com>
2024-06-28 08:56:29 +0900

    [Feat] : 백엔드 서버 초기 세팅

---
+++ assignment-server/src/main/java/io/kr/assignmentserver/AssignmentServerApplication.java
@@ -0,0 +1,13 @@
+package io.kr.assignmentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentServerApplication.class, args);
+    }
+
+}
