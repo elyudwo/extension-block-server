b45c5ecc55738c03b7aac97f410dc2273d97981e
elyudwo <dudqk9696@naver.com>
2024-06-30 15:54:54 +0900

    [Test] : 커스텀확장자를 201개 추가하는 테스트 코드 작성

--- src/test/java/io/kr/assignmentserver/extension/service/ExtensionBlockServiceTest.java
+++ src/test/java/io/kr/assignmentserver/extension/service/ExtensionBlockServiceTest.java
@@ -1,14 +1,12 @@
 package io.kr.assignmentserver.extension.service;
 
 import io.kr.assignmentserver.extension.controller.dto.InsertExtensionDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,5 +36,4 @@ class ExtensionBlockServiceTest {
             }
         });
     }
-
 }
